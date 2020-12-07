75bb601a712a385b4808c8a39fbbcfd3325ed266
Eliel Taskinen <AA3737@student.jamk.fi>
2020-12-07 15:14:11 +0200

    finished osa_1 step 1.14*

--- osa_1/anekdootit/src/index.js
+++ osa_1/anekdootit/src/index.js
@@ -1,37 +1,93 @@
-//step 1.12
+//step 1.14* done, everything resets when you reload the page.
 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const random_number = ({ list }) => {
-
+const random_number = (max) => {
+  var rand_num = Math.floor(Math.random() * max)
+  console.log('rand_num: ', rand_num)
   return (
-    Math.floor(Math.random() * list)
+    rand_num
   )
 }
 
+const increase_points = (func, list, current) => {
+  console.log('func: ', func, 'list: ', list, 'current: ', current)
+  const list_copy = [... list]
+  list_copy[current] += 1
 
-const Button = ({ setSelected}) => {
+  func(list_copy)
+}
+
+
+const Button = ({ func, text }) => {
   return (
     <>
-    <button onClick={() => setSelected(random_number(anectodes))}>
-      Random
+    <button onClick={func}>
+      {text}
     </button>
     </>
   )
 }
 
-const App = (props) => {
-  const [selected, setSelected] = useState(random_number(anecdotes))
+const Votes = ({ votes_list, anec_list }) => {
+
+  var result = 0
+  var pos = 0
+  for (var i= 0; i < votes_list.length; i++) {
+    console.log(votes_list[i])
+    if (votes_list[i] > result) {
+      result = votes_list[i]
+      pos = i
+    }
+  }
+  console.log('result: ', result)
+  console.log('pos: ', pos)
+
+  if (result === 0) {
+    return (
+      <>
+      No votes yet.
+      </>
+    )
+  } else {
+    return (
+      <>
+      <div>
+      {anec_list[pos]}
+      </div>
+      <div>
+      Has {result} votes.
+      </div>
+      </>
+    )
+  }
+}
+
+const App = ({ anec_list }) => {
+  const [selected, setSelected] = useState(0)
+  const [allClicks, setAll] = useState(new Array(anec_list.length).fill(0))
+
 
 
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <div>
+        <h1>Anectode of the day</h1>
+      {anec_list[selected]}
+      </div>
+      <div>Has {allClicks[selected]} points</div>
+      <div>
+        <Button func={() => setSelected(random_number(anec_list.length))} text='Random' />
+        <Button func={() => increase_points(setAll, allClicks, selected)} text='Vote' />
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <Votes votes_list={allClicks} anec_list={anec_list} />
     </div>
   )
 }
+
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -42,7 +98,8 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App anec_list={anecdotes} />,
   document.getElementById('root')
 )
