2875fd31d87474c6aa293f8f2a20ad1d6f764cef
Eliel Taskinen <AA3737@student.jamk.fi>
2020-12-08 23:25:11 +0200

    Finished 2.6

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,26 +1,53 @@
 import React, { useState } from 'react'
 
 
+const BookRender = ({ persons }) => {
+    const person_list = persons.map(person => <p>{person.name}</p>)
+    return (
+        <>
+        {person_list}
+        </>
+    )
+}
 
 
 
 const App = () => {
     const [ persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
     const [ newName, setNewName ] = useState('')
+
+    const handleLineChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+
+    const addLine = (event) =>{
+        event.preventDefault()
+        console.log('button clicked: ', event.target)
+        const phonebookObject = {
+            name: newName
+        }
+
+        setPersons(persons.concat(phonebookObject))
+        setNewName('')
+    }
   
     return (
       <div>
         <h2>Phonebook</h2>
-        <form>
+        <form onSubmit={addLine}>
           <div>
-            name: <input />
+            name: <input 
+            value={newName}
+            onChange={handleLineChange} 
+            />
           </div>
           <div>
             <button type="submit">add</button>
           </div>
         </form>
         <h2>Numbers</h2>
-        ...
+        <BookRender persons={persons} />
       </div>
     )
   
