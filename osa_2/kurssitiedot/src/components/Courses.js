8320ff3380429b8ba0feb2819398580692eed8f8
Eliel Taskinen <AA3737@student.jamk.fi>
2020-12-08 11:57:36 +0200

    started on kurssitiedot osa_2

---
+++ osa_2/kurssitiedot/src/components/Courses.js
@@ -0,0 +1,60 @@
+//This is Courses module
+
+import React from 'react'
+
+
+const Header = (props) => {
+    console.log(props.course)
+    return (
+      <>
+        <h2>
+          {props.course}
+        </h2>
+      </>
+    )
+  }
+  
+  const Content = (props) => {
+      console.log(props)
+      console.log(props.parts)
+      const list_parts = props.parts.map(part => 
+      <p key={part.id}>
+        {part.name} {part.exercises}
+      </p>)
+    return (
+      <>
+        {list_parts}
+      </>
+    )
+  }
+  
+  const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    console.log('total: ', total)
+    return (
+      <strong>
+        Total of {total} exercises.
+      </strong>
+    )
+  }
+  
+  
+  const Course = ({ courses }) => {
+  
+    const courses_render = courses.map(course => 
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+      )
+  
+    return (
+      <>
+      <h1>Web development Curriculum</h1>
+      {courses_render}
+    </>
+    )
+  }
+  
+export default Course
--- osa_2/kurssitiedot/src/index.js
+++ osa_2/kurssitiedot/src/index.js
@@ -1,70 +1,59 @@
-//Done steps 1.1 - 1.5
+//Done steps 2.1, 2.2, 2.3*, 2.4, 2.5
 
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-  console.log(props.course)
-  return (
-    <>
-      <h1>
-        {props.course}
-      </h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-    console.log(props)
-    console.log(props.parts)
-    const list_parts = props.parts.map(part => 
-    <li key={part.id}>
-      {part.name} {part.exec}
-    </li>)
-  return (
-    <ul>
-      {list_parts}
-    </ul>
-  )
-}
-
-
-const Course = ({ course}) => {
-  return (
-    <>
-  <Header course={course.name} />
-  <Content parts={course.parts} />
-  </>
-  )
-}
+import Course from './components/Courses'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Course courses={courses} />
     </div>
   )
 }
